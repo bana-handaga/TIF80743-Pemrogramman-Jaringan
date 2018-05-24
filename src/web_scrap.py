6cd49ccb693c709f6b4906005974a7045044d947
bana-handaga <bana.handaga@ums.ac.id>
2018-05-24 20:42:19 +0700

    Create web_scrap.py

---
+++ src/web_scrap.py
@@ -0,0 +1,10 @@
+UMS_Forlap_link = 'https://forlap.ristekdikti.go.id/perguruantinggi/detail/NkQxMjQxNDItRTc5OC00RjYyLTg3NEItQ0U0MzVCNTQwOUYx'
+
+from bs4 import BeautifulSoup4 as bs
+import urllib3
+L = urllib3.PoolManager()
+html = L.request('GET', UMS_Forlap_link)
+
+if html.status == 200:
+	Ums_html = bs(html.data, 'html.parser')
+	print( ums_html.pritify() )
