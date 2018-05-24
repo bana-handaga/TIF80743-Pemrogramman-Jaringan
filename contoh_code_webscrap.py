830362eb182226965c591bcd5c74ed834c0c5a63
bana-handaga <bana.handaga@ums.ac.id>
2018-05-24 20:52:21 +0700

    Update contoh_code_webscrap.py

--- contoh_code_webscrap.py
+++ contoh_code_webscrap.py
@@ -1,10 +1,19 @@
 UMS_Forlap_link = 'https://forlap.ristekdikti.go.id/perguruantinggi/detail/NkQxMjQxNDItRTc5OC00RjYyLTg3NEItQ0U0MzVCNTQwOUYx'
 
-from bs4 import BeautifulSoup4 as bs
+
+from bs4 import BeautifulSoup as bs
 import urllib3
 L = urllib3.PoolManager()
 html = L.request('GET', UMS_Forlap_link)
 
 if html.status == 200:
-	Ums_html = bs(html.data, 'html.parser')
+	
+	ums_html = bs(html.data, 'html.parser')
+	# cetak dokumen HTML di layar
 	print( ums_html.prettify() )
+	
+	T1 = ums_html.select('table')[0]
+	nama_pt = T1.select('tr')[1].select('td')[2]
+	print ("OUTPUT=================")
+	# cetak nama pt di layar
+	print (nama_pt.string)
